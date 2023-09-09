3a753ba7773a94bfdea236645adbfe0f12d3ae12
ZileXa <zilexa@gmx.com>
2023-09-09 17:27:04 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,14 @@
+sudo rm -r /usr/share/gnome-shell/extensions/desktop-icons@csoriano
+
 # Install script to easily install extensions system wide
 wget -O gnome-shell-extension-installer.sh "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer"
 
 # ArcMenu for all users
-sudo bash /home/asterix/gnome-shell-extension-installer.sh 3628
+#sudo bash /home/asterix/gnome-shell-extension-installer.sh 3628
 # Gesture Improvements for all users
-sudo bash /home/asterix/gnome-shell-extension-installer.sh 4245
+#sudo bash /home/asterix/gnome-shell-extension-installer.sh 4245
 # Dash to Panel for all users
-sudo bash /home/asterix/gnome-shell-extension-installer.sh 1160
+#sudo bash /home/asterix/gnome-shell-extension-installer.sh 1160
 # Bing Wallpaper for all users
 sudo bash /home/asterix/gnome-shell-extension-installer.sh 1160
 # Improved On Screen Keyboard
