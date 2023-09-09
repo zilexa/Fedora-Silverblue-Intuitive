7662b55ca6d520f38d61a0ba894932974d6c1e09
ZileXa <zilexa@gmx.com>
2023-09-09 17:19:44 +0200

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,25 @@
+# Install script to easily install extensions system wide
+wget -O gnome-shell-extension-installer.sh "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer"
+
+# ArcMenu for all users
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 3628
+# Gesture Improvements for all users
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 4245
+# Dash to Panel for all users
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 1160
+# Bing Wallpaper for all users
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 1160
+# Improved On Screen Keyboard
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 4413
+# Allow Locked Remote Desktop
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 4338
+# Gnome UI Tune
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 4158
+# Desktop Icons
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 2087
+# AppIndicator
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 615
+# Custom Hot Corners
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 4167
+# Removable Drive Menu
+sudo bash /home/asterix/gnome-shell-extension-installer.sh 7
