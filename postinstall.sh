70656177d468ffe5c393700d6e440d5c85785ec9
ZileXa <zilexa@gmx.com>
2023-10-11 11:37:47 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,55 +1,9 @@
 #!/bin/bash
 echo "___________________________________________________________________________________"
 echo "                                                                                   "
-echo "                  APPLICATIONS - Apply Firefox global defaults                     "
+echo "                           GET LANGUAGE INFO FROM USER                             "
 echo "___________________________________________________________________________________"
-# For current and future system users and profiles
-# Create default policies (install minimal set of extensions and theme, enable syncing of your toolbar layout, disable default Mozilla bookmarks)
-# first delete existing profiles
-rm -r $HOME/.mozilla/firefox/*.default-release
-rm -r $HOME/.mozilla/firefox/*.default
-rm $HOME/.mozilla/firefox/profiles.ini
-
-# Create a persistent, writeable /usr layer to make changes to the /usr/lib64/firefox folder, applying a policy and defaultPrefs
-sudo rpm-ostree usroverlay --hotfix
-
-# Enable default Firefox config file
-sudo tee -a /usr/lib64/firefox/defaults/pref/autoconfig.js &>/dev/null << EOF
-pref("general.config.filename", "firefox.cfg");
-pref("general.config.obscure_value", 0);
-EOF
-# Create default Firefox config file
-# -Use system default file manager - include toolbar layout in Sync - Enable bookmarks bar - set toolbar layout
-sudo tee -a /usr/lib64/firefox/firefox.cfg &>/dev/null << EOF
-// IMPORTANT: Start your code on the 2nd line
-defaultPref("services.sync.prefs.sync.browser.uiCustomization.state",true);
-defaultPref("media.ffmpeg.vaapi.enabled",true);
-defaultPref("media.ffvpx.enabled",false);
-defaultPref("media.navigator.mediadatadecoder_vpx_enabled",true);
-defaultPref("media.rdd-vpx.enabled",false);
-defaultPref("dom.w3c_touch_events.enabled",1);
-defaultPref("widget.use-xdg-desktop-portal.file-picker",1);
-defaultPref("widget.use-xdg-desktop-portal.mime-handler",1);
-defaultPref("services.sync.prefs.sync.browser.uiCustomization.state",true);
-defaultPref("browser.toolbars.bookmarks.visibility", "always");
-defaultPref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"screenshot-button\",\"print-button\",\"save-to-pocket-button\",\"bookmarks-menu-button\",\"library-button\",\"preferences-button\",\"panic-button\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"downloads-button\",\"ublock0_raymondhill_net-browser-action\",\"urlbar-container\",\"customizableui-special-spring2\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"fxa-toolbar-menu-button\",\"history-panelmenu\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"_d133e097-46d9-4ecc-9903-fa6a722a6e0e_-browser-action\",\"_contain-facebook-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"widget-overflow-fixed-list\",\"PersonalToolbar\"],\"currentVersion\":17,\"newElementCount\":3}");
-EOF
-# Create default firefox policies
-# -Cleanup bookmarks toolbar by disabling default Mozilla bookmarks - install bare minimum extensions
-sudo tee -a /usr/lib64/firefox/distribution/policies.json &>/dev/null << EOF
-{
-  "policies": {
-    "DisableProfileImport": true,
-    "NoDefaultBookmarks": true,
-    "Extensions": {
-      "Install": ["https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/bypass-paywalls-clean/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/facebook-container/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/google-container/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/nord-polar-night-theme/latest.xpi"]
-    }
-  }
-}
-EOF
-
-
-# Get language info
+# Get language & region info
 echo "---------------------------------------"
 echo "Besides English, what other language would you like to spellcheck?" answer
 echo "Please type the 2-letter countrycode for the language you would like to install, for example "de" for German language (no caps):"
@@ -60,12 +14,16 @@ echo "__________________________________________________________________________
 echo "                                                                                   "
 echo "               APPLICATIONS - Install required and recommended apps                "
 echo "___________________________________________________________________________________"
-# Disable Firefox in the base image - it does not allow video playback due to lack of proprietary codecs
-#rpm-ostree override remove firefox-langpacks firefox 
 # Add RPM Fusion to allow for other apps to install, like AMD, INTEL or NVIDIA drivers
 rpm-ostree install --apply-live https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-# Add tools and applications by overlaying the base image
+# Override the default codecs with the non-free codecs for full video playback support in all applications + ffmpeg support
+rpm-ostree override remove libavcodec-free libavfilter-free libavutil-free libavformat-free libswscale-free libswresample-free libpostproc-free --install ffmpeg --install libavcodec-freeworld
+
+# Install essential applications via RPM-OSTREE, apps/tools/extensions 
 rpm-ostree install hunspell-$LANG wireguard-tools dconf-editor gnome-tweaks gnome-screenshot gnome-connections gnome-shell-extension-dash-to-panel.noarch gnome-shell-extension-appindicator.noarch gnome-shell-extension-drive-menu.noarch nemo nemo-extensions nemo-compare nemo-emblems nemo-fileroller nemo-image-converter nemo-search-helpers xed nextcloud-client
+
+# Disable Firefox in the base image - it does not allow video playback due to lack of proprietary codecs
+#rpm-ostree override remove firefox-langpacks firefox 
 
 # add Flathub repo and install remaining apps as flatpaks
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -147,8 +105,9 @@ echo "__________________________________________________________________________
 echo "Configure NEMO file manager"
 echo "__________________________________"
 # Fix the shortcut
-cp /usr/share/applications/nemo.desktop /home/asterix/.local/share/applications/
-sed -i -e 's@OnlyShowIn=X-Cinnamon;Budgie;@#OnlyShowIn=X-Cinnamon;Budgie;@g' /home/asterix/.local/share/applications/nemo.desktop
+sudo mkdir -p /usr/local/share/applications/
+sudo cp /usr/share/applications/nemo.desktop /usr/local/share/applications/
+sed -i -e 's@OnlyShowIn=X-Cinnamon;Budgie;@#OnlyShowIn=X-Cinnamon;Budgie;@g' /usr/local/share/applications/nemo.desktop
 
 # Disable Gnome Nautilus Filemanager
 sudo cp /usr/share/applications/org.gnome.Nautilus.desktop /usr/local/share/applications/
@@ -158,12 +117,11 @@ sudo update-desktop-database /usr/local/share/applications/
 # Associate Nemo as the default filemanager
 # For root
 sudo xdg-mime default nemo.desktop inode/directory
-sudo update-desktop-database /root/.local/share/applications/
 # For current user
 xdg-mime default nemo.desktop inode/directory
 xdg-mime default nemo.desktop x-directory/normal
 xdg-mime default nemo-autorun-software.desktop x-content/unix-software
-update-desktop-database
+update-desktop-database /usr/local/share/applications/
 
 # Set Nemo bookmarks, reflecting folder that will be renamed later (Videos>Media)
 truncate -s 0 $HOME/.config/gtk-3.0/bookmarks
@@ -212,6 +170,52 @@ rm /tmp/libreoffice-profile.tar.xz
 
 echo "Configure FIREFOX"
 echo "_____________________"
+# For current and future system users and profiles
+# Create default policies (install minimal set of extensions and theme, enable syncing of your toolbar layout, disable default Mozilla bookmarks)
+# first delete existing profiles
+rm -r $HOME/.mozilla/firefox/*.default-release
+rm -r $HOME/.mozilla/firefox/*.default
+rm $HOME/.mozilla/firefox/profiles.ini
+
+# Enable default Firefox config file
+sudo tee -a /etc/firefox/pref/autoconfig.js &>/dev/null << EOF
+pref("general.config.filename", "firefox.cfg");
+pref("general.config.obscure_value", 0);
+EOF
+sudo mkdir -p /etc/firefox/defaults/pref
+sudo cp /etc/firefox/pref/autoconfig.js /etc/firefox/defaults/pref/
+# Create default Firefox config file
+# -Use system default file manager - include toolbar layout in Sync - Enable bookmarks bar - set toolbar layout
+sudo tee -a /etc/firefox/firefox.cfg &>/dev/null << EOF
+// IMPORTANT: Start your code on the 2nd line
+defaultPref("services.sync.prefs.sync.browser.uiCustomization.state",true);
+defaultPref("media.ffmpeg.vaapi.enabled",true);
+defaultPref("media.ffvpx.enabled",false);
+defaultPref("media.navigator.mediadatadecoder_vpx_enabled",true);
+defaultPref("media.rdd-vpx.enabled",false);
+defaultPref("dom.w3c_touch_events.enabled",1);
+defaultPref("widget.use-xdg-desktop-portal.file-picker",1);
+defaultPref("widget.use-xdg-desktop-portal.mime-handler",1);
+defaultPref("services.sync.prefs.sync.browser.uiCustomization.state",true);
+defaultPref("browser.toolbars.bookmarks.visibility", "always");
+defaultPref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"screenshot-button\",\"print-button\",\"save-to-pocket-button\",\"bookmarks-menu-button\",\"library-button\",\"preferences-button\",\"panic-button\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"downloads-button\",\"ublock0_raymondhill_net-browser-action\",\"urlbar-container\",\"customizableui-special-spring2\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"fxa-toolbar-menu-button\",\"history-panelmenu\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"_d133e097-46d9-4ecc-9903-fa6a722a6e0e_-browser-action\",\"_contain-facebook-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"widget-overflow-fixed-list\",\"PersonalToolbar\"],\"currentVersion\":17,\"newElementCount\":3}");
+EOF
+# Create default firefox policies
+# -Cleanup bookmarks toolbar by disabling default Mozilla bookmarks - install bare minimum extensions
+sudo mkdir -p /etc/firefox/policies
+sudo tee -a /etc/firefox/policies/policies.json &>/dev/null << EOF
+{
+  "policies": {
+    "DisableProfileImport": true,
+    "NoDefaultBookmarks": true,
+    "Extensions": {
+      "Install": ["https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/bypass-paywalls-clean/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/facebook-container/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/google-container/latest.xpi", "https://addons.mozilla.org/firefox/downloads/latest/nord-polar-night-theme/latest.xpi"]
+    }
+  }
+}
+EOF
+
+
 # Use your custom Firefox Sync Server by default
 echo "---------------------------------------"
 read -p "Would you like to use your own Firefox Sync Server? (y/n)" answer
@@ -235,34 +239,34 @@ echo "Firefox: would you like to be able to launch different profiles (2), by si
 read -p "Only useful if multiple users use this machine and each user has its own Firefox profile. (y/n)?" answer
 case ${answer:0:1} in
     y|Y )
-    cp /usr/share/applications/firefox.desktop /home/asterix/.local/share/applications/
+    sudo cp /usr/share/applications/firefox.desktop /usr/local/share/applications/
     echo "Please enter the first Firefox profile (user) name:"
-    read -p 'firefox profile 1 name (e.g. Lisa): ' profile1
+    read -p 'firefox profile 1 name (e.g. Lisa): ' PROFILE1
     echo "Please enter the second Firefox profile (user) name:"
-    read -p 'firefox profile 2 name (e.g. John): ' profile2
+    read -p 'firefox profile 2 name (e.g. John): ' PROFILE2
     echo adding profiles to right-click of Firefox shortcut... 
-    sed -i -e 's@Actions=new-window;new-private-window;profile-manager-window;@Actions=new-window;$profile1;$profile2;@g' /home/asterix/.local/share/applications/firefox.desktop
-    cat >> /home/asterix/.local/share/applications/firefox.desktop << EOF 
+    sudo sed -i -e 's@Actions=new-window;new-private-window;profile-manager-window;@Actions=new-window;$PROFILE1;$PROFILE2;@g' /usr/local/share/applications/firefox.desktop
+    sudo cat >> /usr/local/share/applications/firefox.desktop << EOF 
 
-[Desktop Action $profile1]
+[Desktop Action $PROFILE1]
 Name=start $profile1's Firefox
-Exec=firefox -P $profile1 -no-remote
+Exec=firefox -P $PROFILE1 -no-remote
 
-[Desktop Action $profile2]
+[Desktop Action $PROFILE2]
 Name=start $profile2's Firefox
-Exec=firefox -P $profile2 -no-remote
+Exec=firefox -P $PROFILE2 -no-remote
 
 EOF
 
     # The shortcut in ~/.local/share/application overrides the system shortcuts in /usr/share/applications. This also removes file associations. Fix those:
-    xdg-settings set default-web-browser firefox.desktop
-    xdg-mime default firefox.desktop x-scheme-handler/chrome
-    xdg-mime default firefox.desktop application/x-extension-htm
-    xdg-mime default firefox.desktop application/x-extension-html
-    xdg-mime default firefox.desktop application/x-extension-shtml
-    xdg-mime default firefox.desktop application/xhtml+xml
-    xdg-mime default firefox.desktop application/x-extension-xhtml
-    xdg-mime default firefox.desktop application/x-extension-xht
+    ##xdg-settings set default-web-browser firefox.desktop
+    ##xdg-mime default firefox.desktop x-scheme-handler/chrome
+    ##xdg-mime default firefox.desktop application/x-extension-htm
+    ##xdg-mime default firefox.desktop application/x-extension-html
+    ##xdg-mime default firefox.desktop application/x-extension-shtml
+    ##xdg-mime default firefox.desktop application/xhtml+xml
+    ##xdg-mime default firefox.desktop application/x-extension-xhtml
+    ##xdg-mime default firefox.desktop application/x-extension-xht
     ;;
     * )
         echo "Keeping the Firefox shortcut as is..."
